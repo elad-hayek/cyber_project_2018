ab2e940d8bb67745eb759ecea4daff965d396910
elad3283@gmail.com <zxcvvcxz!123>
2018-01-26 18:12:55 +0200

    made the creation of a ahk entry with 1 function that receive shortcut type

--- shortcuts.py
+++ shortcuts.py
@@ -2,6 +2,8 @@
 import subprocess
 import os
 
+STARTUP_FOLDER_PATH = 'C:\Users\USER\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup'
+
 CLOSE_PROCESS_BY_NAME_PATH = 'E:\USER\Documents\school\cyber\project_2018\user_shortcuts\close_process.ahk'
 
 HOT_KEYS_PROGRAM_PATH = 'E:\Program Files\AutoHotKey\AutoHotkey.exe'
@@ -28,6 +30,7 @@ if GetKeyState("{0}") & GetKeyState("{1}") & GetKeyState("{2}") & GetKeyState("{
 class ShortCuts:
     def __init__(self):
         self.__shortcut_sequence = ''
+        self.__shortcut_script_path = ''
         self.__shortcuts_templates = {'open folder': 'Run, %s',
                                       'open url': 'Run, chrome.exe %s',
                                       'open settings': 'Run, control %s',
@@ -37,103 +40,63 @@ class ShortCuts:
         self.__current_shortcuts = {'open folder': {}, 'open url': {}, 'open program': {},
                                     'open cmd': {}, 'open settings': {}}
 
-        self.__files_ending_counter = {'folders': 0, 'urls': 0, 'programs': 0}
+        self.__files_ending_counter = {'folder': 0, 'url': 0, 'program': 0, 'cmd': 0, 'settings': 0}
 
+        self.__scripts_path = {'folder': FOLDER_SCRIPTS_PATH, 'url': URL_SCRIPTS_PATH, 'program': PROGRAM_SCRIPTS_PATH,
+                                    'cmd': CMD_SCRIPTS_PATH, 'settings': SETTINGS_SCRIPTS_PATH}
 
+#-------------------------------------------------------------------------------
     def get_shortcut_sequence(self):
         self.__shortcut_sequence = raw_input('enter your shortcut sequence:')
         self.__shortcut_sequence = '+'.join(self.__shortcut_sequence).split('+')
 
-    def open_folder(self):
-        path = raw_input('enter path:')
-        ahk_file = open(FOLDER_SCRIPTS_PATH+r'\folder'+str(          # create new ahk file
-            self.__files_ending_counter['folders'])+'.ahk', 'w')
+#-------------------------------------------------------------------------------
+    def run_action_sequence_for_shortcut(self, shortcut_type, argument=''):
+        self.__shortcut_script_path = self.__scripts_path[shortcut_type]+'\\'+shortcut_type+str(self.__files_ending_counter[shortcut_type])+'.ahk'
+        print self.__shortcut_script_path
+        ahk_file = open(self.__shortcut_script_path, 'w')
 
-        ahk_file.write(self.write_to_file('open folder', path))
+        ahk_file.write(self.write_to_file('open '+shortcut_type, argument))
         ahk_file.close()
 
 
-        self.activate_ahk_files(FOLDER_SCRIPTS_PATH+r'\folder'+str(         # activate
-            self.__files_ending_counter['folders'])+'.ahk')
+        self.activate_ahk_files(self.__shortcut_script_path)
 
-        self.add_to_history('open folder', r'folder'+str(self.__files_ending_counter['folders'])+'.ahk',FOLDER_SCRIPTS_PATH+r'\folder'+str(self.__files_ending_counter['folders'])+'.ahk', path)
+        self.add_to_history('open '+shortcut_type, shortcut_type+str(self.__files_ending_counter[shortcut_type])+'.ahk', argument)
 
-        self.__files_ending_counter['folders'] += 1
+        self.__files_ending_counter[shortcut_type] += 1
 
-    def open_url(self):
-        url = raw_input('enter url: ')
-        ahk_file = open(URL_SCRIPTS_PATH+'\url'+str(                # create new ahk file
-            self.__files_ending_counter['urls'])+'.ahk', 'w')
-
-        ahk_file.write(self.write_to_file('open url', url))
-        ahk_file.close()
-
-        self.activate_ahk_files(URL_SCRIPTS_PATH+'\url'+str(                # activate
-            self.__files_ending_counter['urls'])+'.ahk')
-
-        self.add_to_history('open url', r'url'+str(self.__files_ending_counter['urls'])+'.ahk', URL_SCRIPTS_PATH+'\url'+str(self.__files_ending_counter['urls'])+'.ahk' ,url)
-
-        self.__files_ending_counter['urls'] += 1
-
-    def open_program(self):
-        path = raw_input('enter path:')
-        ahk_file = open(PROGRAM_SCRIPTS_PATH+r'\program'+str(          # create new ahk file
-            self.__files_ending_counter['programs'])+'.ahk', 'w')
-
-        ahk_file.write(self.write_to_file('open program', path))
-        ahk_file.close()
-
-
-        self.activate_ahk_files(PROGRAM_SCRIPTS_PATH+r'\program'+str(       # activate
-            self.__files_ending_counter['programs'])+'.ahk')
-
-        self.add_to_history('open program', r'program'+str(self.__files_ending_counter['programs'])+'.ahk', PROGRAM_SCRIPTS_PATH+r'\program'+str(self.__files_ending_counter['programs'])+'.ahk', path)
-
-        self.__files_ending_counter['programs'] += 1
-
-    def open_cmd(self):
-        ahk_file = open(CMD_SCRIPTS_PATH+r'\cmd.ahk', 'w')
-
-        ahk_file.write(self.write_to_file('open cmd'))
-        ahk_file.close()
-
-        self.activate_ahk_files(CMD_SCRIPTS_PATH+r'\cmd.ahk')
-
-        self.add_to_history('open cmd', r'cmd.ahk', CMD_SCRIPTS_PATH+r'\cmd.ahk')
-
-    def open_settings(self):
-        ahk_file = open(SETTINGS_SCRIPTS_PATH+r'\settings.ahk', 'w')
-
-        ahk_file.write(self.write_to_file('open settings'))
-        ahk_file.close()
-
-        self.activate_ahk_files(SETTINGS_SCRIPTS_PATH+r'\settings.ahk')
-
-        self.add_to_history('open settings', r'settings.ahk',SETTINGS_SCRIPTS_PATH+r'\settings.ahk')
-
+#-------------------------------------------------------------------------------
 
     def write_new_shortcut(self, shortcut_name):
         self.get_shortcut_sequence()
+
         if shortcut_name == 'open folder':
-            self.open_folder()
+            path = raw_input('enter path:')
+            self.run_action_sequence_for_shortcut('folder', path)
 
         elif shortcut_name == 'open url':
-            self.open_url()
+            url = raw_input('enter url:')
+            self.run_action_sequence_for_shortcut('url', url)
 
         elif shortcut_name == 'open program':
-            self.open_program()
+            path = raw_input('enter path:')
+            self.run_action_sequence_for_shortcut('program', path)
 
         elif shortcut_name == 'open cmd':
-            self.open_cmd()
+            self.run_action_sequence_for_shortcut('cmd')
 
         elif shortcut_name == 'open settings':
-            self.open_settings()
+            self.run_action_sequence_for_shortcut('settings')
+
+#-------------------------------------------------------------------------------
 
     def write_to_file(self, shortcut_name, argument=''):
         sequence_format_list = self.check_sequence_length()
-        string_to_write = HOT_KEYS_TEMPLATE.format(*sequence_format_list)+'\n{'+ self.__shortcuts_templates[shortcut_name] % argument +'\n}'
+        string_to_write = HOT_KEYS_TEMPLATE.format(*sequence_format_list)+'\n{'+ self.__shortcuts_templates[shortcut_name] % argument + '\n}'
         return string_to_write
 
+#-------------------------------------------------------------------------------
 
     def check_sequence_length(self):
         sequence_format_list = self.__shortcut_sequence[:]
@@ -141,20 +104,23 @@ class ShortCuts:
             for i in range(6-len(sequence_format_list)):
                 sequence_format_list.append(sequence_format_list[-1])
         return sequence_format_list
-
+#-------------------------------------------------------------------------------
     def delete_shortcut(self, shortcut_type, file_name):
         file_to_delete = self.__current_shortcuts[shortcut_type][file_name][2]
         self.activate_ahk_files(CLOSE_PROCESS_BY_NAME_PATH, file_name)
         os.remove(file_to_delete)
-
+#-------------------------------------------------------------------------------
+    def add_to_startup(self):
+        pass
+#-------------------------------------------------------------------------------
     def show_current_shortcuts(self):
         print self.__current_shortcuts
-
+#-------------------------------------------------------------------------------
     def activate_ahk_files(self, file_path, argument=''):
         subprocess.Popen([HOT_KEYS_PROGRAM_PATH, file_path, argument])
-
-    def add_to_history(self, shortcut_name, file_name, full_path, argument=None):
-        self.__current_shortcuts[shortcut_name][file_name] = (argument, self.__shortcut_sequence, full_path)
+#-------------------------------------------------------------------------------
+    def add_to_history(self, shortcut_type, file_name, argument=None):
+        self.__current_shortcuts[shortcut_type][file_name] = (argument, self.__shortcut_sequence, self.__shortcut_script_path)
 
 
 def main():
