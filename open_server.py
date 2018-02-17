28d231e2243fd9a8594b7d0cf2137ea6ab0da9f8
elad3283@gmail.com <zxcvvcxz!123>
2018-02-17 18:20:33 +0200

    made the server loop endlessly

--- open_server.py
+++ open_server.py
@@ -4,23 +4,21 @@ from new_computer_class import *
 import sys
 
 def main():
+    server_type = sys.argv[1]
+    server = Server(int(server_type))
     try:
-        server_type = sys.argv[1]
-        with open('a.txt', 'w') as f:
-            f.write('aaaaaaaaaaaaaaaaaaaaaaaaaaaa       '+server_type)
         while 1:
-            server = Server(int(server_type))
+            server.connect_to_client()
             shortcut_information = server.receive_information_from_client()
-            print shortcut_information
             server.pass_information_to_client('ok')
             if int(server_type) == 0:
                 server.make_the_shortcut_file(shortcut_information[0], shortcut_information[1], shortcut_information[2])
             server.disconnect_client()
-            server.close_server()
+
     except Exception, e:
-        pass
-        # with open('a.txt', 'w') as f:
-        #     f.write(repr(e))
+        with open('a.txt', 'w') as f:
+            f.write(repr(e))
+        server.close_server()
 
 if __name__ == '__main__':
     main()
