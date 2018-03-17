65b617c19bcaebe63a5b58a92450044cc8479592
elad3283@gmail.com <zxcvvcxz!123>
2018-03-17 15:20:23 +0200

    fixed the ip address the acting client gets when connecting to linux server

--- activate_remote_data.py
+++ activate_remote_data.py
@@ -5,18 +5,23 @@ from subprocess import *
 from new_computer_class import *
 
 
+
 def main():
+
     action = sys.argv[1]
     argument = sys.argv[2]
     mac = sys.argv[3]
 
     arp_question = Popen(['python', 'get_ip_and_mac.py', mac], stdout=PIPE)
     result = arp_question.communicate()[0]
-    ip = result
+    ip = result.strip()
 
     client = Client()
     client.connect_to_server(ip, SERVER_ACTING_PORT)
     client.activate_the_shortcut_on_the_computer(action, argument)
+
+    with open('a.txt', 'w') as f:
+        f.write(client.receive_information_from_the_server())
 
     client.close_client()
 
