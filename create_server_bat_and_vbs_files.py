603c8049dc800d1aa5b8423fd66a10918669c8f2
elad3283@gmail.com <zxcvvcxz!123>
2018-05-18 11:10:20 +0300

    deleted the copy of the hotkey program

--- create_server_bat_and_vbs_files.py
+++ create_server_bat_and_vbs_files.py
@@ -66,13 +66,6 @@ class Create_bat_and_vbs_files:
             self.create_acting_server_bat()
             self.create_acting_server_vbs()
             self.activate_servers()
-            self.copy_the_autohotkey_program_to_startup()
-
-    def copy_the_autohotkey_program_to_startup(self):
-        """
-        copy the autohotkey program to the startup folder
-        """
-        shutil.copy2(AUTOHOTKEY_PROGRAM_PATH, CREATING_SCRIPTS_PATH)
 
     def crate_listening_server_bat(self):
         """
